18cb174a54ed784629f609ebacd80fb84cc111fc
JaCrowe <jacrowe@mta.ca>
2021-11-02 23:15:18 -0300

    create vue project and home page

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,22 @@
 <template>
-  <div id="nav">
+  <!-- <div id="nav">
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </div>
-  <router-view />
+  <router-view /> -->
+  <Home />
 </template>
+<script lang="ts">
+import { defineComponent } from "vue";
+import Home from "@/views/Home.vue"; // @ is an alias to /src
 
+export default defineComponent({
+  name: "App",
+  components: {
+    Home,
+  },
+});
+</script>
 <style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
