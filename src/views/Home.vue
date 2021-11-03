d04bb4ee224905e1b92d9de8dbc452985d4b78bb
JaCrowe <jacrowe@mta.ca>
2021-11-03 10:05:07 -0300

    broke the rotation by switching to buffer material. Nice lol

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
-  <h1>Jeremy Crowe</h1>
   <div ref="container" class="container" />
+  <div class="content"><h1>Jeremy Crowe</h1></div>
 </template>
 
 <script lang="ts">
@@ -9,52 +9,172 @@ import * as THREE from "three";
 
 export default defineComponent({
   name: "Home",
-  mounted() {
-    let camera: THREE.Camera, scene: THREE.Scene, renderer: THREE.WebGLRenderer;
-    let geometry: THREE.BoxGeometry, material: THREE.Material, mesh: THREE.Mesh;
+  data() {
+    return {
+      plainVertex: `
+          varying vec2 vUv;
 
-    const init = () => {
-      camera = new THREE.PerspectiveCamera(
-        70,
-        window.innerWidth / window.innerHeight,
-        0.01,
-        10
-      );
-      camera.position.z = 1;
+          void main()	{
 
-      scene = new THREE.Scene();
+            vUv = uv;
 
-      geometry = new THREE.BoxGeometry(0.2, 0.2, 0.2);
-      material = new THREE.MeshNormalMaterial();
+            gl_Position = vec4( position, 1.0 );
 
-      mesh = new THREE.Mesh(geometry, material);
-      scene.add(mesh);
+          }
+        `,
+      swirlyFragment: `
+          varying vec2 vUv;
 
-      renderer = new THREE.WebGLRenderer({ antialias: true });
-      renderer.setSize(window.innerWidth / 4, window.innerHeight / 4);
-      renderer.setAnimationLoop(animation);
+          uniform float time;
 
-      (this.$refs.container as HTMLDivElement).appendChild(renderer.domElement);
+          void main()	{
+
+            vec2 p = - 1.0 + 2.0 * vUv;
+            float a = time * 40.0;
+            float d, e, f, g = 1.0 / 40.0 ,h ,i ,r ,q;
+
+            e = 400.0 * ( p.x * 0.5 + 0.5 );
+            f = 400.0 * ( p.y * 0.5 + 0.5 );
+            i = 200.0 + sin( e * g + a / 150.0 ) * 20.0;
+            d = 200.0 + cos( f * g / 2.0 ) * 18.0 + cos( e * g ) * 7.0;
+            r = sqrt( pow( abs( i - e ), 2.0 ) + pow( abs( d - f ), 2.0 ) );
+            q = f / r;
+            e = ( r * cos( q ) ) - a / 2.0;
+            f = ( r * sin( q ) ) - a / 2.0;
+            d = sin( e * g ) * 176.0 + sin( e * g ) * 164.0 + r;
+            h = ( ( f + d ) + a / 2.0 ) * g;
+            i = cos( h + r * p.x / 1.3 ) * ( e + e + a ) + cos( q * g * 6.0 ) * ( r + h / 3.0 );
+            h = sin( f * g ) * 144.0 - sin( e * g ) * 212.0 * p.x;
+            h = ( h + ( f - e ) * q + sin( r - ( a + h ) / 7.0 ) * 10.0 + i / 4.0 ) * g;
+            i += cos( h * 2.3 * sin( a / 350.0 - q ) ) * 184.0 * sin( q - ( r * 4.3 + a / 12.0 ) * g ) + tan( r * g + h ) * 184.0 * cos( r * g + h );
+            i = mod( i / 5.6, 256.0 ) / 64.0;
+            if ( i < 0.0 ) i += 4.0;
+            if ( i >= 2.0 ) i = 4.0 - i;
+            d = r / 350.0;
+            d += sin( d * d * 8.0 ) * 0.52;
+            f = ( sin( a * g ) + 1.0 ) / 2.0;
+            gl_FragColor = vec4( vec3( f * i / 1.6, i / 2.0 + d / 13.0, i ) * d * p.x + vec3( i / 1.3 + d / 8.0, i / 2.0 + d / 18.0, i ) * d * ( 1.0 - p.x ), 1.0 );
+          }
+        `,
     };
+  },
+  methods: {
+    setupShaderScene() {
+      let camera: THREE.Camera,
+        scene: THREE.Scene,
+        renderer: THREE.WebGLRenderer;
+      let geometry: THREE.PlaneGeometry,
+        material: THREE.Material,
+        mesh: THREE.Mesh;
 
-    function animation(time: number) {
-      mesh.rotation.x = time / 2000;
-      mesh.rotation.y = time / 1000;
+      let uniforms = { time: { value: 0 } };
 
-      renderer.render(scene, camera);
-    }
+      const init = () => {
+        const aspect = window.innerWidth / window.innerHeight;
+        const girth = 1;
+        camera = new THREE.OrthographicCamera(
+          -girth,
+          girth,
+          -girth / aspect,
+          girth / aspect,
+          0.01,
+          10
+        );
+        camera.position.z = 1;
 
-    init();
+        scene = new THREE.Scene();
+
+        geometry = new THREE.PlaneGeometry(girth * 2, girth * 2, 4);
+        material = new THREE.ShaderMaterial({
+          uniforms,
+          vertexShader: this.plainVertex,
+          fragmentShader: this.swirlyFragment,
+        });
+
+        mesh = new THREE.Mesh(geometry, material);
+        scene.add(mesh);
+
+        renderer = new THREE.WebGLRenderer({ antialias: true });
+        renderer.setSize(window.innerWidth, window.innerHeight);
+        renderer.setAnimationLoop(animation);
+
+        (this.$refs.container as HTMLDivElement).appendChild(
+          renderer.domElement
+        );
+      };
+
+      function animation(time: number) {
+        uniforms.time.value += 0.01;
+        renderer.render(scene, camera);
+      }
+
+      init();
+    },
+    setupNormalSceneLol() {
+      let camera: THREE.Camera,
+        scene: THREE.Scene,
+        renderer: THREE.WebGLRenderer;
+      let geometry: THREE.PlaneBufferGeometry,
+        material: THREE.Material,
+        mesh: THREE.Mesh;
+      let uniforms = { time: { value: 0 } };
+
+      const init = () => {
+        camera = new THREE.PerspectiveCamera(
+          70,
+          window.innerWidth / window.innerHeight,
+          0.01,
+          10
+        );
+        camera.position.z = 1;
+        scene = new THREE.Scene();
+        geometry = new THREE.PlaneBufferGeometry(0.2, 0.2, 1);
+        material = new THREE.ShaderMaterial({
+          uniforms,
+          vertexShader: this.plainVertex,
+          fragmentShader: this.swirlyFragment,
+        });
+        mesh = new THREE.Mesh(geometry, material);
+        scene.add(mesh);
+        renderer = new THREE.WebGLRenderer({ antialias: true });
+        renderer.setSize(window.innerWidth, window.innerHeight);
+        renderer.setAnimationLoop(animation);
+        (this.$refs.container as HTMLDivElement).appendChild(
+          renderer.domElement
+        );
+      };
+      function animation(time: number) {
+        mesh.rotation.x = time / 2000;
+        mesh.rotation.y = time / 1000;
+        uniforms.time.value += 0.01;
+
+        renderer.render(scene, camera);
+      }
+      init();
+    },
+  },
+  mounted() {
+    this.setupNormalSceneLol();
   },
 });
 </script>
 
 <style lang="scss">
 .container {
-  width: 100%;
-  margin: auto;
-  display: flex;
-  align-items: center;
-  justify-content: center;
+  width: 100vw;
+  height: 100vh;
+  position: fixed;
+  top: 0;
+  left: 0;
+  margin: 0;
+  padding: 0;
+  z-index: 1;
+}
+.content {
+  position: relative;
+  z-index: 2;
+  h1 {
+    color: white;
+  }
 }
 </style>
